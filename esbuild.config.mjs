767e9f31373df88c5e1456392fb43800fc5916ec
mrgratz <77765913+mrgratz@users.noreply.github.com>
2026-08-28 18:29:05 -0700

    Add os to esbuild externals (operator-panel.ts's os.tmpdir() call broke the build)

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -10,7 +10,7 @@ esbuild.build({
   entryPoints: ["src/main.ts"],
   bundle: true,
   external: [
-    "obsidian", "electron", "child_process", "fs",
+    "obsidian", "electron", "child_process", "fs", "os",
     "@codemirror/autocomplete", "@codemirror/collab", "@codemirror/commands",
     "@codemirror/language", "@codemirror/lint", "@codemirror/search",
     "@codemirror/state", "@codemirror/view",
